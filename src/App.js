a4fae11c6a7a2d875e080b797075d56a382c42cb
vinaykhade <khadevnay.me@gmail.com>
2021-11-20 22:04:36 -0800

    socket and chat integration

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://localhost:4001";
+import { io } from "socket.io-client";
+import ChatWindow from './components/ChatWindow';
+import Home from './components/Home';
+
+
+// const ENDPOINT = "http://localhost:4001";
+const ENDPOINT = "http://172.24.9.112:4001";
+
+
 
 function App() {
 
   const [response, setResponse] = useState("");
+  const [socket, setSocket] = useState(null);
+  const [socketId, setSocketId] = useState('');
 
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("FromAPI", data => {
-      setResponse(data);
-    });
+    const socketIO = io(ENDPOINT);
+    socketIO.on("connect", () => {
+      setSocketId(socketIO.id);
+      setSocket(socketIO);
+    });    
 
-
-    return () => socket.disconnect();
+    return () => socketIO.disconnect();
   }, []);
 
   return (
     <div className="App">
-      <p>
-        <input placeholder="Enter user ID to start chatting with your connected peers"/>
-        <h4 style={{textAlign: "center"}}>OR</h4>
-        <Link to="/chat">Click to Create New User ID</Link>
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/chat' element={<ChatWindow 
+                                          socket={socket} 
+                                          socketId={socketId}
+                                          ENDPOINT={ENDPOINT} 
+                                          />} 
+                                        />
+        </Routes>
+      </BrowserRouter> 
     </div>
   );
 }
